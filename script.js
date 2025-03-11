eca84dc0e447fff7ad219b29cd9ac08dc6f4efdf
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-11 13:45:41 -0300

    Remove arquivo script.js contendo a funcionalidade de alternância do menu de hambúrguer

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-hamburguer").addEventListener("click", function () {
-    var links = document.getElementById("navbar-links");
-    if (links.style.display === "block") {
-        links.style.display = "none";
-    } else {
-        links.style.display = "block";
-    }
-});
