b2a1f2f0b0a62858d10d68bd46034b55b46091da
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-13 12:04:12 -0300

    Adiciona arquivos no service worker

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,24 @@ const CACHE_NAME = 'ghibli-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
+  './css/style.css',
+  './css/mobile.css',
+  '/manifest.json',
+  './assets/logo.ico',
+  './assets/logo.png',
+  './assets/parallax/imagem1.png',
+  './assets/parallax/imagem2.jpg',
+  './assets/parallax/imagem3.jpg',
+  './assets/parallax/imagem4.png',
+  './assets/parallax/imagem5.jpg',
+  './assets/seta-para-cima.png',
+  './assets/icons/android/android-launchericon-48-48.png',
+  './assets/icons/android/android-launchericon-72-72.png',
+  './assets/icons/android/android-launchericon-96-96.png',
+  './assets/icons/android/android-launchericon-144-144.png', 
+  './assets/icons/android/android-launchericon-192-192.png',
+  './assets/icons/android/android-launchericon-512-512.png',
+  './assets/icons/ios/256.png',
 ];
 
 // Instalando o Service Worker e armazenando em cache
