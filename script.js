46bb37fa65e96bacdc6f7dcf1aedf0522846c0c7
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-02 16:10:25 -0300

    Remove implementação de menu móvel e estilos associados; limpa estrutura HTML

--- script.js
+++
@@ -1,8 +0,0 @@
-function menuMobile() {
-    let menuHamburguer = document.querySelector('.menu-mobile');
-    if (menuHamburguer.classList.contains('open')) {
-        menuHamburguer.classList.remove('open');
-    } else {
-        menuHamburguer.classList.add('open');
-    }
-}
